cc30d31d1c74d15dc23cc4e4d871afdfa5bed6e7
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-10-01 21:06:45 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'meuprojeto';
+  nome = 'Mateus';
+
+  adiciona() {
+    const numero = Math.round(Math.random() * 100);
+    this.nome = `Mateus ${numero}`;
+    console.log(`Adicionado ${this.nome}`);
+  }
+
+  /*
+  adiciona(nome){
+ console.log(nome.value);
+  }
+  Variavel por referencia
+  */
+
+  alteraNome(evento) { // metodo
+    this.nome = evento.target.value;
+  }
+
+  isDisabled(): boolean{ // metodo
+    return this.nome.length <= 2;
+  }
+
+
+
 }
